348e8c3c3138f3e3f1758e082fc9ab351c443659
berklast <Ahmetmuhammetbenli@gmail.com>
2025-05-08 23:38:24 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,17 +4,17 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDH5GBWQNoZv7LZZ2MbFjh-twI1jZuYqK0",
-    authDomain: "newdc-d6404.firebaseapp.com",
-    projectId: "newdc-d6404",
-    storageBucket: "newdc-d6404.appspot.com",
-    messagingSenderId: "101292652984",
-    appId: "1:101292652984:web:59d6b49b400572bec1774d",
-    measurementId: "G-NV86JDVJ27"
+  apiKey: "AIzaSyDH5GBWQNoZv7LZZ2MbFjh-twI1jZuYqK0",
+  authDomain: "newdc-d6404.firebaseapp.com",
+  projectId: "newdc-d6404",
+  storageBucket: "newdc-d6404.appspot.com", // .appspot.com olarak düzeltildi
+  messagingSenderId: "101292652984",
+  appId: "1:101292652984:web:59d6b49b400572bec1774d",
+  measurementId: "G-NV86JDVJ27"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db };
+export { auth, db }; // Diğer dosyalarda kullanmak için export
